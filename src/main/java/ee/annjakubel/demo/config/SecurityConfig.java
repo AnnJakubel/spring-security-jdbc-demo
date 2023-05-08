e5a21bfca9d2e2a49d7dd09a837abb3b009fdcce
AnnJakubel <annabel.jakubel@gmail.com>
2023-05-08 13:50:50 +0300

    Initial commit

---
+++ src/main/java/ee/annjakubel/demo/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package ee.annjakubel.demo.config;
+
+public class SecurityConfig {
+}
---
+++ src/test/java/ee/annjakubel/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.annjakubel.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
